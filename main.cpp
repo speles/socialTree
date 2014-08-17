63c3f8d7590a632c6e2b19de8fd7b0e86eb52249
speles@mail.ua <speles@mail.ua>
2014-08-18 00:14:50 +0300

    Fixed QT_NO_OPENGL behaviour

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <QApplication>
+#include <QLabel>
 #include <QTimer>
 #include <QObject>
 
 #ifndef QT_NO_OPENGL
 #include <opengl/mainwidget.h>
-#endif
 #include "request/request.h"
 #include "request/requestprovider.h"
 #include "core/corehandler.h"
 #include "opengl/AnimationManager.h"
+#endif
 
 #include <iostream>
 
@@ -21,13 +22,13 @@ int main(int argc, char *argv[])
 #ifndef QT_NO_OPENGL
     widget = new MainWidget();
     widget->show();
-#else
-    QLabel note("OpenGL Support required");
-    note.show();
-#endif
 
     coreHandler* handler = new coreHandler();
     RequestProvider* reqProvider = new RequestProvider(handler);
     animationManager = new AnimationManager(handler);
+#else
+    QLabel note("OpenGL Support required");
+    note.show();
+#endif
     return app.exec();
 }
